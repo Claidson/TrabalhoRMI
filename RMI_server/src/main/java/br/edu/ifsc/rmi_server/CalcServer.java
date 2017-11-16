8289d84a9c493be7c68bcdcc81f5746bf9bf3fc3
Claidson <cchaidrich@hotmail.com>
2017-11-16 09:56:32 -0200

    Funcionando

--- RMI_server/src/main/java/br/edu/ifsc/rmi_server/CalcServer.java
+++ RMI_server/src/main/java/br/edu/ifsc/rmi_server/CalcServer.java
@@ -14,14 +14,16 @@ import java.rmi.registry.Registry;
  * @author aluno
  */
 public class CalcServer {
+
     public static void main(String[] args) {
         try {
-            Registry reg = LocateRegistry.createRegistry(1099);
+            Registry reg = LocateRegistry.createRegistry(50000);
             CalculadoraApp calc = new CalculadoraApp();
             reg.rebind("calculadora", calc);
             System.out.println("Servidor calculadora pronto...");
-            
+
         } catch (Exception ex) {
+            System.out.println("Erro" + ex);
         }
     }
 }
